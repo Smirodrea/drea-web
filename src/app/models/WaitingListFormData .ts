2ed54ea35ef0079081473c8aaf8f35c2e611cbf9
Arik <arikshalito@gmail.com>
2024-02-19 18:56:53 +0200

    w00t

--- src/app/models/WaitingListFormData .ts
+++
@@ -1,7 +0,0 @@
-export class WaitingListFormData {
-  fullName: string = '';
-  brokerageName: string | null = ''; // Optional field, can be null
-  email: string = '';
-  phone: string | null = ''; // Optional field, can be null
-  thoughts: string | null = ''; // Optional field, can be null
-}
